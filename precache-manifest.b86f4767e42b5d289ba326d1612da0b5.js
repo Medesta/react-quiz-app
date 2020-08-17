2f08f93ce92ecfe02a08d0b96e6e1bc1dc6705e9
Mehdi Raza Kazi <mehdi.raza@koderlabs.com>
2020-08-17 20:36:43 +0500

    Updates

---
+++ precache-manifest.b86f4767e42b5d289ba326d1612da0b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f13011186eec4d7591cb6172ba1eee",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "4e5ff42fef0dc4b5e779",
+    "url": "/react-quiz-app/static/css/main.22d09011.chunk.css"
+  },
+  {
+    "revision": "b3f474bc4e1750db5268",
+    "url": "/react-quiz-app/static/js/2.b47c6e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz-app/static/js/2.b47c6e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e5ff42fef0dc4b5e779",
+    "url": "/react-quiz-app/static/js/main.d30c0ec6.chunk.js"
+  },
+  {
+    "revision": "f61cd5cfa6b969211628",
+    "url": "/react-quiz-app/static/js/runtime-main.c8169479.js"
+  }
+]);
